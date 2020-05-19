2b56710e1a73cba1257d6658229d4dacd9b5bbd0
Ahvic <antoineroblou@gmail.com>
2020-05-19 14:33:54 +0200

    Ajout d'une couche service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CreeProjetComponent } from './cree-projet/cree-projet.component';
+import { ServiceImpl } from './services/ServiceImpl';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreeProjetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ServiceImpl
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cree-projet/cree-projet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cree-projet',
+  templateUrl: './cree-projet.component.html',
+  styleUrls: ['./cree-projet.component.scss']
+})
+export class CreeProjetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
